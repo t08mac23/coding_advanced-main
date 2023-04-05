fbe9b0a2ed321563bd3b0d959e3395809f68919e
t08mac23 <t08mac23@gmail.com>
2023-04-05 17:02:41 +0900

    ヘッダーとスワイパー

--- coding_advanced-main/index.js
+++ coding_advanced-main/index.js
@@ -1,1 +1,18 @@
-const swiper = new Swiper(".swiper", {});
+const swiper = new Swiper(".swiper", {
+  slidesPerView: 2,
+  // ページネーション
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // 自動
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  // ナビボタン
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
