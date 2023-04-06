3dfac6551fe5fcec3b2dbe5e085fa8803487d868
t08mac23 <t08mac23@gmail.com>
2023-04-06 12:38:01 +0900

    body1, top

--- coding_advanced-main/index.js
+++ coding_advanced-main/index.js
@@ -15,4 +15,16 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
   }
+  // スクロールバー
+});
+
+$(function () {
+  $('.detail').hide();
+  $('.info').on('click', function () {
+    // クリックした要素の ID と違うクラス名のセクションを非表示
+    $('.detail').not($('.'+$(this).attr('id'))).hide();
+    // クリックした要素の ID と同じクラスのセクションを表示
+    $('.'+$(this).attr('id')).toggle();
+    // $('.detail').toggle();
+  });
 });
